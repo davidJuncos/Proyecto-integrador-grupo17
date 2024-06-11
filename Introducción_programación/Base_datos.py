f33039870b09190b466c644d015e8d7676f441c9
johnmack10 <john200430@gmail.com>
2024-06-10 21:14:17 -0300

    Agg base de datos

---
+++ Introducción_programación/Base_datos.py
@@ -0,0 +1,24 @@
+import mysql.connector
+
+HOST = "localhost"
+USER = "root"
+PASSWORD = "Palaancha1"
+BD = "hoteles"#nacidos
+
+# Conectarse a la base de datos
+conn = mysql.connector.connect(
+    host=HOST,
+    user=USER,
+    password=PASSWORD,
+    database=BD
+)
+
+# Crear un cursor
+cursor = conn.cursor()
+
+print(conn)
+
+def cerrarConexion():
+    cursor.close()
+    conn.close()
+    
--- Introducción_programación/crud_personal.py
+++ Introducción_programación/crud_personal.py
@@ -1,5 +1,34 @@
+import Base_datos
+def mostrar_roles():
+    query = "SELECT * FROM Roles WHERE Rol LIKE '%%'"
+    
+    Base_datos.cursor.execute(query)
+    roles = Base_datos.cursor.fetchone()
+
+    print(Base_datos.cursor.rowcount, "Filas afectadas")
+
+    if(Base_datos.cursor.rowcount >= 1):
+        print(roles)
+        return True
+    else:
+        print(f"No existe en la base de datos roles del personal")
+        return False
+
+
 def agregar_personal ():
-    print("AGREGADO")
+    Nombre = input("Ingrese nombre: ")
+    Apellido = input("Ingrese apellido: ")
+    Horainicia = float(input("Ingrese horario de inicio: "))
+    HoraFin = float(input("Ingrese horario de salida: "))
+    mostrar_roles()
+    IdRol = input("Ingrese rol: ")
+    query = "INSERT INTO Personal (Nombre, Apellido, HoraInicia, HoraFin, Estado, IdRol) VALUES (%s, %s, %s, %s, %s, %s)"
+    values = (Nombre, Apellido, Horainicia, HoraFin, 1,IdRol)
+    
+    Base_datos.cursor.execute(query, values)
+    Base_datos.conn.commit() 
+    print("Persona insertada con éxito.")
+
 
 def modificar_personal ():
     print("MODIFICADO")
@@ -8,5 +37,19 @@ def eliminar_personal ():
     print("ELIMINADO")
 
 def mostrar_personal ():
-    pass
+    query = "SELECT * FROM Personal WHERE id = %s"
+    values = (id,)
+    Base_datos.cursor.execute(query,values)
+    personaUnica = Base_datos.cursor.fetchone()
+
+    print(Base_datos.cursor.rowcount, "Filas afectadas")
+
+    if(Base_datos.cursor.rowcount == 1):
+        print(personaUnica)
+        return True
+    else:
+        print(f"No existe en la base de datos la persona con id: {id}")
+        return False
+
+print(agregar_personal())
 
