6f9ed71b2d485a4a8859f6498f1a5b879e474b3c
John Mackinson <john200430@gmail.com>
2024-09-03 17:41:04 -0300

    Agregado aritmetica Gise

--- evidencia1/app/test_aritmetica.py
+++ evidencia1/app/test_aritmetica.py
@@ -1,5 +1,47 @@
 import aritmetica
 
+def test_sumar(a,b):
+   print("Suma...")
+   
+   suma = a + b
+   assert suma == 12  
+   assert 3 != 2 
+   assert -1009,"Error "
+   assert -1
+   assert not False," Falso "
+   print ("test completo")
+      
+def test_restar(a,b):
+   print("Resta..." ) 
+   
+   resta= a-b 
+   assert resta == 1 
+   assert resta!=6
+   assert a>b
+   assert b<a
+   assert resta >=1
+   assert resta <=5
+   print("test completo")
+          
+def test_dividir(a,b):
+   
+   print("Dividir...")
+   dividir = a / b
+   try:
+      1/0
+   except ZeroDivisionError:
+    pass
+   else:
+     assert False
+   assert dividir > 0
+   assert not (dividir < 0)
+   print("test completo")
+   
+     
+if __name__=="__main__":
+ test_sumar(5,7)
+ test_restar(6,5)
+ test_dividir(4,2)
 def test_multiplicar():
     assert aritmetica.multiplicar(3, 4.567) == 13.70
     assert aritmetica.multiplicar(0, 10) == 0.00
