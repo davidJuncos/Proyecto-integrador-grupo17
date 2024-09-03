5190df176a157ea4f6e7e4b711aaa2d78a05377c
Mar-Urzag <maria.g.urzagaste@outlook.com>
2024-09-03 09:55:37 -0300

    cambios recomendados

--- aritmetica.py
+++ aritmetica.py
@@ -1,43 +1,26 @@
 #Iniciando el punto 3
 #suma
-def suma_numeros() ->float:
-    print("Sumar")
-    a = float(input("numero:  "))
-    b = float(input("numero:  "))
-    suma= a + b
-    print(f"Resultado: {suma}") 
-    return suma
-
-   #llamar a la funcion
-suma_numeros()
+def suma_numeros(a,b) ->float:
+ suma= a + b
+ return suma
 
 #resta
-def restar_numeros() ->float:
-    print("Restar")
-    a = float(input("número: "))
-    b = float(input("número: "))
-    resta= a - b
-    print(f"Resultado: {resta}") 
-    return resta
-#llamar resta
-restar_numeros()
-
-#dividir
+def restar_numeros(a,b) ->float:
+ resta= a - b
+ return resta 
+ #dividir
+#parametrisar
 # Manejando errores:Para manejar (capturar) las excepciones podemos usar 
 #un bloque de código con las palabras reservada try and except
 #Aquel código que se encuentre dentro del bloque try se ejecutará normalmente siempre y cuando haya un error
 #Si se produce una excepción, ésta será capturada por el bloque except, ejecutandose el código que contiene.
-def dividir_numeros() -> float:
-    print("Division")
-    a = float(input("número: "))
-    b = float(input("número: "))
-    
+def dividir_numeros(a,b) -> float:
+   
     try:
         dividir = a / b
-        print(f"Resultado: {dividir}")
-        return dividir
+        
+        return dividir 
+   
     except ZeroDivisionError:
-        print(" Error en la Division ")
-        return None
-#LLamar a dividir
-dividir_numeros()
+      raise ValueError(" Error en la Division ")
+      return None
--- test_aritmetica.py
+++ test_aritmetica.py
@@ -1,52 +1,49 @@
 #Iniciando punto 4
 #utilizar aserciones nativas
 # test_sumar
-
-def test_sumar():
-      print("Suma...")
-      a = 5
-      b = 7
-      suma = a + b
-      assert suma == 12  
-      assert 3 != 2 
-      assert -1009,"Error "
-      assert -1
-      assert not False," Falso "
-      print ("test completo")
+# input y pasarle los parametro 
+def test_sumar(a,b):
+   print("Suma...")
+   
+   suma = a + b
+   assert suma == 12  
+   assert 3 != 2 
+   assert -1009,"Error "
+   assert -1
+   assert not False," Falso "
+   print ("test completo")
       
 #test_restar
-def test_restar():
-      print("Resta...")
-      a = 10
-      b = 9
-      resta= a-b 
-      assert resta == 1 
-      assert resta!=6
-      assert a>b
-      assert b<a
-      assert resta >=1
-      assert resta <=5
-      print("test completo")
+def test_restar(a,b):
+   print("Resta..." ) 
+   
+   resta= a-b 
+   assert resta == 1 
+   assert resta!=6
+   assert a>b
+   assert b<a
+   assert resta >=1
+   assert resta <=5
+   print("test completo")
           
 #test_dividir
-def test_dividir():
-     print("Dividir...")
-     a= 45
-     b= 5
-     dividir = a / b
-     try:
-        1/0
-     except ZeroDivisionError:
-        pass
-     else:
-        assert False
-     assert dividir > 0
-     assert not (dividir < 0)
-     print("test completo")
+def test_dividir(a,b):
+   
+   print("Dividir...")
+   dividir = a / b
+   try:
+      1/0
+   except ZeroDivisionError:
+    pass
+   else:
+     assert False
+   assert dividir > 0
+   assert not (dividir < 0)
+   print("test completo")
    
      
 if __name__=="__main__":
- test_sumar()
- test_restar()
- test_dividir()
+ test_sumar(5,7)
+ test_restar(6,5)
+ test_dividir(4,2)
  
