90962311666d273c632ad3568067b30cfee9c73f
John Mackinson <john200430@gmail.com>
2024-09-03 19:40:08 -0300

    sumado de assert

--- evidencia1/app/test_aritmetica.py
+++ evidencia1/app/test_aritmetica.py
@@ -37,18 +37,13 @@ def test_dividir(a,b):
    assert not (dividir < 0)
    print("test completo")
    
-     
-if __name__=="__main__":
- test_sumar(5,7)
- test_restar(6,5)
- test_dividir(4,2)
 def test_multiplicar():
     assert aritmetica.multiplicar(3, 4.567) == 13.70
-    assert aritmetica.multiplicar(0, 10) == 0.00
+    assert aritmetica.multiplicar(0, 12323) == 0.00
     assert aritmetica.multiplicar(-2, 3) == -6.00
 
 def test_sumar_n():
-    assert aritmetica.sumar_n(1, 2, 3) == 6.00
+    assert aritmetica.sumar_n(1, 2, 1, 1, 5) == 10.00
     assert aritmetica.sumar_n(1.5, 2.5, 3.5) == 7.50
     assert aritmetica.sumar_n(-1, -2, -3) == -6.00
 
@@ -56,8 +51,12 @@ def test_promedio_n():
     assert aritmetica.promedio_n(1, 2, 3) == 2.00
     assert aritmetica.promedio_n(1.5, 2.5, 3.5) == 2.50
     assert aritmetica.promedio_n(-1, -2, -3) == -2.00
+    assert aritmetica.promedio_n(10, 1, 10, -3) == 4.50
 
 if __name__ == "__main__":
+    test_sumar(5,7)
+    test_restar(6,5)
+    test_dividir(4,2)
     test_multiplicar()
     test_sumar_n()
     test_promedio_n()
