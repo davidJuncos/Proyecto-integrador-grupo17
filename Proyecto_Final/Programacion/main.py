b30252ff8516902e2da99a7c4aea896a46b69baa
John Mackinson <john200430@gmail.com>
2024-11-01 00:38:21 -0300

    Cambio Base de datos

--- Proyecto_Final/Programacion/main.py
+++ Proyecto_Final/Programacion/main.py
@@ -1,25 +1,50 @@
 # main.py
+import subprocess
+import os
 from menus import menu_usuarios, menu_graficos
 from usuario import Usuario, Acceso
 from registros_pluviales import RegistrosPluviales
 import os
 from datetime import datetime
 
+
 def main():
+    
+    #SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
+    
+    
+    #BASE_DATOS_DIR = os.path.join(SCRIPT_DIR, '..',"Proyecto_Final/Bd")
+    #FILE_NAME_LOGS_BASE_DE_DATOS = os.path.join(BASE_DATOS_DIR,"Back_CRUD")
     registros = None  # Inicializar variable para almacenar registros pluviales
 
     while True:
         print("\n--- Menú Principal ---")
         print("1. Menú de Usuarios")
-        print("2. Cargar Registros Pluviales")
-        print("3. Menú de Gráficos")
+        print("2. Ingresar al sistema con los datos de usuario")
+        print("3. Análisis de datos")
         print("4. Salir")
+        
         opcion = input("Selecciona una opción: ")
 
         if opcion == '1':
             menu_usuarios()
+        
+        elif opcion == "2": 
+            #print("Accediendo al Menú de Base de Datos.")
+             # Accede a Back_CRUD.py como un proceso independiente
+            try:
+                # Define la ruta completa a Back_CRUD.py               
+                pathbackcrud = os.path.join(os.path.dirname(__file__), '..', 'BD', 'Back_CRUD1.py')
+                
+            except FileNotFoundError as e:
+                print(f"Error: {e}")
+                print("a")
+            except Exception as e:
+                print(f"Ocurrió un error al cambiar de directorio: {e}")
+           #FILE_NAME_LOGS_BASE_DE_DATOS,menu1()
+           #FILE_NAME_LOGS_BASE_DE_DATOS,main1()
 
-        elif opcion == '2':
+        elif opcion == '3':
             try:
                 anio = int(input("Ingresa el año para cargar registros pluviales: "))
                 registros = RegistrosPluviales.cargar_o_generar_registros(anio)
@@ -31,15 +56,19 @@ def main():
                     print("Mes no válido. Ingresa un número entre 1 y 12.")
             except ValueError:
                 print("Error: Debes ingresar un número válido para el año y el mes.")
-
-        elif opcion == '3':
+                
             if registros:
                 menu_graficos(registros)
             else:
                 print("Primero debes cargar los registros pluviales antes de generar gráficos.")
-
-        elif opcion == '4':
-            print("Saliendo de la aplicación.")
+        elif opcion == '3':
+            if registros:
+                menu_graficos(registros)
+            else:
+                print("Primero debes cargar los registros pluviales antes de generar gráficos.")    
+           
+        elif opcion == "4": 
+            print("Saliendo del menú de usuarios.")
             break
 
         else:
